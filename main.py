9a3e60ede4b56655dfb0f487d7b6d2d706b86ec2
KaranRohra <rohrakaran38@gmail.com>
2024-08-25 00:15:30 +0530

    Changed Default TImezone to Asia/Kolkata

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+import os
+import time
+
 from oms import trading
+from utils.constants import Env
 
 
 def main():
+    os.environ["TZ"] = "Asia/Kolkata"
+    if os.environ[Env.MACHINE_OS] in ("ubuntu",):
+        time.tzset()
     trading.start()
 
 
